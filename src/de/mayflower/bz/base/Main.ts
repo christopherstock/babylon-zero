d89d048b54a63f8156db6f97d0548a8bcb7d0e4e
Christopher Stock <email@christopherstock.de>
2018-05-09 22:53:52 +0200

    Updated TODO list. Added some bg sounds.

--- src/de/mayflower/bz/base/Main.ts
+++ src/de/mayflower/bz/base/Main.ts
@@ -10,32 +10,37 @@
     *   TODO make param for mesh's physics attributes (increase player mass!)!
     *   TODO Remove all ids for meshes, textures and materials? Use Auto-Counters?
     *   TODO MeshFactory enums to separate file MeshProps.
+    *   TODO Assign player size from settings constants.
     *   TODO make game objects (static wall, non-static movable, player etc.)
     *   TODO Create (different?) characters from primitives or mesh objects?
-    *   TODO Solve shaking on colliding and walking against walls!
     *   TODO Create 3d gun as 2nd scene in front?
+    *   TODO Collisions via shooting.
+    *   TODO Bullet holes.
+    *   TODO Solve shadows?
+    *   TODO Add creators for more mesh primitives!
     *   TODO Check Perfect Dark and Goldeneye sound board.
     *   TODO Solve correct tiling for boxes (faceUVs, backUVs).
+    *   TODO Create debug console on-screen.
+    *   TOOD Try GUI via babylon-gui.
     *   TODO Try fog or smoke?
+    *   TODO Create simple test level with increased performance!
+    *   TODO Try to create a realistic scene.
     *   TODO Fix skybox and link to player!
     *   TODO Smooth camera animation on active camera change?
     *   TODO Auto-Calc mass according to mesh volume: Calculate mass for boxes according to their density. Create density value!
-    *   TODO Assign player size from settings constants.
+    *   TODO Solve shaking on colliding and walking against walls!
     *   TODO Try to solve player smooth sliding on any walls!
     *   TODO Enable jumping!
     *   TODO Specify, how deep the player rect may sink into colliding objects. (0 would be perfect!) ?
     *   TODO Delete LevelBunny. Move all functonality to LevelTest.
+    *   TODO Improve performance in chrome? Try explicitly setting 'webGL 1.0'??
     *   TODO Add player/camrera controls (turn, duck)
     *   TODO Solve player falling into infinity (camera stops at .. ?)
     *   TODO Enrich and correct all documentation.
     *   TODO Solve free rotations physics for camera object (on rotated planes or boxes?)
     *   TODO Catch mouse in window in browser?? https://www.html5rocks.com/en/tutorials/pointerlock/intro/
+    *   TODO Improve mesh system.
     *   TODO Solve ortho drawing!
-    *   TOOD Try GUI via babylon-gui.
-    *   TODO Try to create a realistic scene.
-    *   TODO Create debug console on-screen.
-    *   TODO Try fur.
-    *   TODO Improve performance in chrome? Try webGL 1.0??
     *   TODO Show FPS output as Ortho drawing: bz.MfgInit.engine.getFps().toFixed() + " fps"
     *   TODO Increase performance in chrome?
     *   TODO Create material system with unified parameters!
@@ -43,18 +48,14 @@
     *   TODO Improve Sprite System handling.
     *   TODO move onInitLevelCompleted to class Level and also scene to class level?
     *   TODO specify explicit gravity for physics engine and scene?
-    *   TODO Add creators for more mesh primitives!
-    *   TODO Improve mesh system.
+    *   TODO Try fur.
     *   TODO Enable wearpon zoom. (view angle / camera solution?)
-    *   TODO Collisions via shooting..
-    *   TODO Bullet holes.
-    *   TODO Solve shadows?
-    *   TODO Create simple test level with increased performance!
     *   TODO Improve abstract level system and make it more generic.
     *   TODO Try PostProcess (camera.setPostProcess ?)
     *   TODO Create main menu where player can reset level etc.
     *   TODO Solve 3dsmax OBJ file importer? ( with different OBJ file? )
-    *   TODO try dynamic textures ( video in texture in front of screen .. )
+    *   TODO Try 3dsmax 2018/2019 with babylon plugin?
+    *   TODO try dynamic textures ( video in texture in front of screen for company presentation site .. )
     *   TODO Review babylon.JS tutorials, features and playground.
     *******************************************************************************************************************/
     export class Main
