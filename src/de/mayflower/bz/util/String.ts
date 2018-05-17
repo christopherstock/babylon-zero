22e43d8b69bc3d4c3bcccbea4e049f9ccf57aade
Christopher Stock <email@christopherstock.de>
2018-05-17 19:15:20 +0200

    Enriching and revising documentation tags in progress.

--- src/de/mayflower/bz/util/String.ts
+++ src/de/mayflower/bz/util/String.ts
@@ -2,7 +2,7 @@
     const moment:any = require('moment');
 
     /*******************************************************************************************************************
-    *   Offers static string functionality.
+    *   Offers extended string functionality.
     *******************************************************************************************************************/
     export class String
     {
@@ -11,10 +11,10 @@
         *   The subject will need the 'multiple' modifier for this method to work as expected.
         *   e.g. /[a-z]+/g
         *
-        *   @param  subject  The target string to apply the regular expression search on.
-        *   @param  regEx    The regular expression.
-        *                    This string MUST NOT be enclosed in string quotes!
-        *   @return          An array containing all matched results.
+        *   @param subject The target string to apply the regular expression search on.
+        *   @param regEx   The regular expression.
+        *                  This string MUST NOT be enclosed in string quotes!
+        *   @return        An array containing all matched results.
         ***************************************************************************************************************/
         public static searchRegEx( subject:string, regEx:RegExp ) : string[]
         {
