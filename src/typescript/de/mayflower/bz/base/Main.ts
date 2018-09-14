b5b80a827fc3fe20e47bead79b025de3c617e35e
Christopher Stock <email@christopherstock.de>
2018-09-14 09:06:29 +0200

    Rearranged TODO list.

--- src/typescript/de/mayflower/bz/base/Main.ts
+++ src/typescript/de/mayflower/bz/base/Main.ts
@@ -8,32 +8,58 @@
     *
     *   TODO Create air friction for ALL movables (with a sphere phyyics impostor) in render() of scene! (s. red square)
     *   TODO Enable fully ready 3d models with texture, materials, physics impostors and physics values?
+    *   TODO Improve performance in chrome? Try explicitly setting 'webGL 1.0'??
+    *   TODO Check and fix control delay gap in firefox. (edge, chrome?) (babylon-engine is running smoothly!)
+    *   TODO Smooth edges for bullet holes? Certain bullet hole shapes look white?
+    *   TODO Improve Scene selection and cam selection (replace F1-F5, 1-5) with dropdowns/debug console etc.
+    *   TODO Reduce static accesses to Main.game?
+    *   TODO Load all 3d objects into a separate scene and copy to separate staged scene?
+    *   TODO Smooth camera transfers / animation on changine active camera?
+    *   TODO Make sprites collidable?
+    *   TODO Solve different GUI text sizes that keep their constant size on canvas resize?
+    *   TODO Create main menu where player can reset stage etc.
+    *   TODO Enable altering the pivot for imported models?
     *
-    *   TODO Advanced topics:
+    *   TODO Player physics
     *   =====================
     *   TODO Enable running via SHIFT key.
+    *   TODO Add head shaking on walking.
+    *   TODO Enable jumping? Determine player floor collision ...
+    *   =====================
+    *
+    *   TODO HID
+    *   =====================
     *   TODO Catch mouse in window in browser?? https://www.html5rocks.com/en/tutorials/pointerlock/intro/
+    *   TODO Add wearpon panning (pan view/angle on moving crosshair to the screen border)?
+    *   =====================
+    *
+    *   TODO Sounds
+    *   =====================
     *   TODO Create and test spatial sounds (bullet hit). https://doc.babylonjs.com/how_to/playing_sounds_and_music
     *   TODO Sounds for shot, bullet hit and bullet drop.
-    *   TODO Also zoom the wearpon in the HUD on zooming with the wearpon.
-    *   TODO Particle FX (physics) for wall rubble! Add particle system (fx) for bullet holes' wall rubble.
-    *   TODO Enable wearpon system with different wearpons and attributes: magazine size, shoot angles, range, zoom ..
-    *   TODO Add explosion sprites.
-    *   TODO Enable jumping? Determine player floor collision ...
-    *   TODO Add head shaking on walking.
-    *   TODO Quick fire animation in bullet hole (for certain materials..)?
+    *   TODO Check Perfect Dark and Goldeneye sound board.
+    *   =====================
+    *
+    *   TODO FX
+    *   =====================
     *   TODO Add physically correct bullet drop out after fire.
-    *   TODO Improve performance in chrome? Try explicitly setting 'webGL 1.0'??
-    *   TODO Create wearpons with projectiles (exploding or non-exploding)..
-    *   TODO Add wearpon panning (pan view/angle on moving crosshair to the screen border)?
-    *   TODO Smooth edges for bullet holes? Certain bullet hole shapes look white?
-    *   TODO Check and fix control delay gap in firefox. (edge, chrome?) (babylon-engine is running smoothly!)
-    *   TODO Improve Scene selection and cam selection (replace F1-F5, 1-5) with dropdowns etc.
-    *   TODO Zoom HUD wearpon on zooming?
+    *   TODO Try particle system (fx, non-physicsl) for bullet holes' wall rubble.
+    *   TODO Particle FX (physical) for wall rubble!
+    *   TODO Quick fire animation in bullet hole (for certain materials..)?
+    *   TODO Add explosion sprites.
     *   TODO Create 3d gun as 2nd scene in front?
-    *   TODO Check 'gamelets' in the babylon.JS docs.
-    *   TODO Reduce static accesses to Main.game?
     *   TODO Add muzzle flash to gun on firing.
+    *   =====================
+    *
+    *   TODO Wearpons
+    *   =====================
+    *   TODO Also zoom the wearpon in the HUD on zooming with the wearpon.
+    *   TODO Enable wearpon system with different wearpons and attributes: magazine size, shoot angles, range, zoom ..
+    *   TODO Enable wearpon items (wearpons and ammo) to be picked up.
+    *   TODO Create wearpons with projectiles (exploding or non-exploding)..
+    *   =====================
+    *
+    *   =====================
     *   TODO Complete MVP Tech-Demo 'The Office', 1st minor, still life etc.
     *   =====================
     *
@@ -47,31 +73,29 @@
     *   TODO Try to keep original physic imposter parameters after the compound has been set.
     *   =====================
     *
+    *   TODO Characters
+    *   =====================
     *   TODO Create (different?) characters from primitives or mesh objects??
     *   TODO Create class Bot that represents an enemy with one field for attitude.
     *   TODO Create class Character, the abstract class of Player and Bot.
     *   TODO Checkout babylon.JS bones and skeletons?
     *   TODO Check cannonJs examples ? (human.js .. bones etc.)
+    *   =====================
     *
-    *   TODO Load all 3d objects into a separate scene and copy to separate staged scene?
-    *   TODO Smooth camera transfers / animation on changine active camera?
-    *   TODO Make sprites collidable?
-    *   TODO Solve different GUI text sizes that keep their constant size on canvas resize?
-    *   TODO Check method for disposing all scene members (scene.dispose()) again ?
-    *   TODO Create main menu where player can reset stage etc.
-    *   TODO Enable altering the pivot for imported models?
-    *   TODO Check Perfect Dark and Goldeneye sound board.
-    *
+    *   TODO The Lab
+    *   =====================
     *   TODO Try PostProcessing (camera.setPostProcess ?)
-    *   TODO try dynamic textures: ( video in texture in front of screen for company presentation site .. )
+    *   TODO Try video textures (pc screen etc.)!
+    *   TODO Try dynamic textures: ( video in texture in front of screen for company presentation site .. )
     *   TODO Try 3D GUI again ( requires babylon.JS 3.3 ).
     *   TODO Try fog or smoke?
     *   TODO Try player objects: physics for car, etc?
     *   TODO Try fur.
-    *   TODO Try video textures!
+    *   TODO Try 'gamelets' in the babylon.JS docs.
     *   TODO Try 360 Photodome? http://doc.babylonjs.com/how_to/360photodome
-    *
     *   TODO Review babylon.JS tutorials and gamelets, features and playgrounds for gathering new features.
+    *   =====================
+    *
     *******************************************************************************************************************/
     export class Main
     {
