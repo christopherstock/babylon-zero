beb934816f6bff374d7f3f41dc24d3c4c7a6d3a6
Christopher Stock <christopher.stock@mayflower.de>
2021-11-29 15:33:53 +0100

    Solve static ModelSystem.loadModel.

--- src/typescript/bz/game/engine/scene/model/ModelSystem.ts
+++ src/typescript/bz/game/engine/scene/model/ModelSystem.ts
@@ -37,74 +37,14 @@ export class ModelSystem
     *   TODO maybe try to load not ALL models but only on demand? => blocking version of
     *        BABYLON.SceneLoader.ImportMesh via async await?
     *******************************************************************************************************************/
-    public async load( scene:BABYLON.Scene ) : Promise<any>
+    public async load( scene:BABYLON.Scene ) : Promise<void>
     {
         bz.Debug.init.log( ' Import [' + String( this.fileNames.length ) + '] model files' );
 
         for ( const fileName of this.fileNames )
         {
-            // TODO extract to separate method?
-
-            const fullPath      :string = ( bz.SettingResource.PATH_MODEL + fileName );
-            const lastSeparator :number = fullPath.lastIndexOf( '/' );
-            const directory     :string = fullPath.substr( 0, lastSeparator + 1 );
-            const file          :string = fullPath.substr( lastSeparator + 1 );
-
-            bz.Debug.init.log( ' Import model file ' + file );
-
-            const createdModel :(bz.Model|void) = await BABYLON.SceneLoader.ImportMeshAsync
-            (
-                // first parameter specifies the name of the mesh to import - empty string imports all meshes
-                '',
-                directory,
-                file,
-                scene
-            ).then(
-
-                (
-                    data :any
-                ) => {
-
-                    console.log( 'Mesh loaded successfully!' );
-
-                    const importedMeshes:BABYLON.AbstractMesh[]     = data.meshes;
-                    const particleSystems :BABYLON.ParticleSystem[] = data.particleSystems;
-                    const skeletons :BABYLON.Skeleton[]             = data.skeletons;
-                    const animationGroups :BABYLON.AnimationGroup[] = data.animationGroups;
-
-                    bz.Debug.init.log(
-                        '  Model file ' + file + ' imported. '
-                        + 'Mesh count: ' + String( importedMeshes.length )
-                    );
-
-                    // hide all meshes
-                    for ( const importedMesh of importedMeshes )
-                    {
-                        importedMesh.isVisible = false;
-
-                        // disable backface culling by default
-                        if ( importedMesh.material !== null )
-                        {
-                            importedMesh.material.backFaceCulling = false;
-                        }
-                    }
-
-                    // save in models array
-                    const newModel :bz.Model = new bz.Model( importedMeshes as any );
-                    newModel.extractPhysicsImpostors();
-
-                    return newModel;
-                }
-
-            ).catch(
-                ( reason:any ) => {
-                    bz.Debug.init.err( 'ERROR on model import [' + file + ']' );
-                    bz.Debug.init.err( reason );
-                }
-            );
-
             // assign created model
-            this.models[ fileName ] = createdModel;
+            this.models[ fileName ] = await ModelSystem.loadModel( scene, fileName );
         }
 
         bz.Debug.init.log( '  Model import complete [' + String( this.fileNames.length ) + '] files' );
@@ -112,6 +52,69 @@ export class ModelSystem
         bz.Debug.init.log( 'All models (pre)loaded!' );
 
         this.onLoadComplete();
+    }
+
+    public static async loadModel( scene:BABYLON.Scene, fileName:string ) : Promise<bz.Model|void>
+    {
+        const fullPath      :string = ( bz.SettingResource.PATH_MODEL + fileName );
+        const lastSeparator :number = fullPath.lastIndexOf( '/' );
+        const directory     :string = fullPath.substr( 0, lastSeparator + 1 );
+        const file          :string = fullPath.substr( lastSeparator + 1 );
+
+        bz.Debug.init.log( ' Import model file ' + file );
+
+        const createdModel :(bz.Model|void) = await BABYLON.SceneLoader.ImportMeshAsync
+        (
+            // first parameter specifies the name of the mesh to import - empty string imports all meshes
+            '',
+            directory,
+            file,
+            scene
+        ).then(
+
+            (
+                data :any
+            ) => {
+
+                console.log( 'Mesh loaded successfully!' );
+
+                const importedMeshes:BABYLON.AbstractMesh[]     = data.meshes;
+                const particleSystems :BABYLON.ParticleSystem[] = data.particleSystems;
+                const skeletons :BABYLON.Skeleton[]             = data.skeletons;
+                const animationGroups :BABYLON.AnimationGroup[] = data.animationGroups;
+
+                bz.Debug.init.log(
+                    '  Model file ' + file + ' imported. '
+                    + 'Mesh count: ' + String( importedMeshes.length )
+                );
+
+                // hide all meshes
+                for ( const importedMesh of importedMeshes )
+                {
+                    importedMesh.isVisible = false;
+
+                    // disable backface culling by default
+                    if ( importedMesh.material !== null )
+                    {
+                        importedMesh.material.backFaceCulling = false;
+                    }
+                }
+
+                // save in models array
+                const newModel :bz.Model = new bz.Model( importedMeshes as any );
+                newModel.extractPhysicsImpostors();
+
+                return newModel;
+            }
+
+        ).catch(
+            ( reason:any ) => {
+                bz.Debug.init.err( 'ERROR on model import [' + file + ']' );
+                bz.Debug.init.err( reason );
+            }
+        );
+
+        return createdModel;
     }
 
     /** ****************************************************************************************************************
